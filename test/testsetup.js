1e0711ed7839b7a0dc4099f377503ae5e46be024
clr <clr@port49.com>
2009-04-24 18:24:49 -0400

    -- updated the testing framework for integration tests, Readme to reflect as much --

--- test/testsetup.js
+++ test/testsetup.js
@@ -23,6 +23,29 @@ libraries = function(){
   ];
 };
 
+templates = function(){
+  return [
+    'alt',
+    'basic',
+    'bork',
+    'bork2',
+    'compact',
+    'complex',
+    'compressed',
+    'expanded',
+    'import',
+    'importee',
+    'line_numbers',
+    'mixins',
+    'multiline',
+    'nested',
+    'parent_ref',
+    '_partial',
+    'script',
+    'units'
+  ];
+};
+
 loadJavascriptLibrary = function( root ){
   for( i = 0; i < libraries().length; i++ ){
     var library = document.createElement( 'script' );
@@ -37,17 +60,31 @@ loadJavascriptLibrary( root );
 
 $( function(){
 
-  $( [ '' ].concat( libraries( root ) ) ).each( function( i, test ){ 
+  // Create the unit test selector.
+  $( '#banner' ).append( $( "<span style='color:white;'>unit tests: </span><select id='unit_test_select'></select>" ) );
+  $( [ '' ].concat( libraries() ) ).each( function( i, test ){ 
     var option = $( '<option>'+test+'</option>' );
-    $( '#test_select' ).append( option );
+    $( '#unit_test_select' ).append( option );
     $( option ).data( 'url', document.location.toString().split( "sassijs\/test", 1 )[0] + 'sassijs/test/' + test + '.qunit' );
   });
-
-  $( '#test_select' ).change( function(){
-    var url = $( $( '#test_select option:selected' )[0] ).data( 'url' );
+  $( '#unit_test_select' ).change( function(){
+    var url = $( $( '#unit_test_select option:selected' )[0] ).data( 'url' );
     if( url.length > 0 ){
       document.location = url;
     }
   });
 
+  // Create the template-based integration test selector.
+  $( '#banner' ).append( $( "<span style='color:white;'> integration tests: </span><select id='integration_test_select'></select>" ) );
+  $( [ '' ].concat( templates() ) ).each( function( i, test ){ 
+    var option = $( '<option>'+test+'</option>' );
+    $( '#integration_test_select' ).append( option );
+    $( option ).data( 'url', document.location.toString().split( "sassijs\/test", 1 )[0] + 'sassijs/test/integrations.qunit#' + test );
+  });
+  $( '#integration_test_select' ).change( function(){
+    var url = $( $( '#integration_test_select option:selected' )[0] ).data( 'url' );
+    if( url.length > 0 ){
+      document.location = url;
+    }
+  });
 })
